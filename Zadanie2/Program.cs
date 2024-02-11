41d1febbb1d0cf3f858deb99c1a0a98d493e7a55
Zemskov6 <zemskovivan2005@gmail.com>
2024-02-11 23:46:04 +1000

    Задача 2

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите координаты по оси X");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты по оси Y");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+	System.Console.WriteLine("1");
+}
+if (x < 0 && y > 0)
+{
+	System.Console.WriteLine("2");
+}
+if (x < 0 && y < 0)
+{
+	System.Console.WriteLine("3");
+}
+if (x > 0 && y < 0)
+{
+	System.Console.WriteLine("4");
+}
