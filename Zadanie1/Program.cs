fe18ad0d30b5da228d1f0bfc56e76558af2dec3a
Zemskov6 <zemskovivan2005@gmail.com>
2024-02-11 23:37:17 +1000

    Задача 1

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n%7==0 && n%23==0){
+	System.Console.WriteLine("Число кратно 7 и 23");
+}else{
+	System.Console.WriteLine("Число не кратно 7 и 23");
+}
+
