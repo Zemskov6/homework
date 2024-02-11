55d08565540fcfad71223aff5cfab8fca5de5c92
Zemskov6 <zemskovivan2005@gmail.com>
2024-02-11 23:52:37 +1000

    Задача 3

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число на отрезке [10;99]");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>=10 && n<=99){
+	int n1 = n/10;
+	int n2 = n%10;
+	if (n1>n2){
+		System.Console.WriteLine(n1);
+	}
+	else{
+		System.Console.WriteLine(n2);
+	}
+}
+else{
+	System.Console.WriteLine("Число вне отрезка");
+}
